e79f75885c4a0302d1927581104afd097c62b4f2
Malachai Rossiter <malachai.rossiter@gmail.com>
2023-04-21 18:56:59 -0400

    connected mysql with express server

--- server/server.js
+++ server/server.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const db = require('./models');
 
 app.use(cors({credentials: true, origin: 'http://localhost:3000'}));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.listen(8000, () => console.log(`Listening on port: 8000`));
+const {Recipe} = require('./models');
+
+app.post("/insert", (req, res) => {
+    Recipe.create(req.body)
+    .then(recipe => {
+        res.json(recipe);
+    })
+    .catch((err) => {console.log(err)})
+})
+
+
+//connect to the database and start the server
+db.sequelize.sync().then(() => {
+    console.log(`Connected to the Database`);
+    app.listen(8000, () => console.log(`Listening on Port: 8000`));
+}).catch((err) => {
+    console.log(`Failed to connect to the database`, err);
+})
