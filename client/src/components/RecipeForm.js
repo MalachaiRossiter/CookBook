33a1da073bfccd0445147acd4d4469b0475cc2dc
Malachai Rossiter <malachai.rossiter@gmail.com>
2023-05-10 23:00:58 -0400

    added css reset

--- client/src/components/RecipeForm.js
+++ client/src/components/RecipeForm.js
@@ -1,7 +1,31 @@
 import { useState, useEffect} from 'react';
 
+import '../styles/RecipeForm.css';
+
 const RecipeForm = (props) => {
     const [formData, setFormData] = useState({
-        
-    })
+        id: '',
+        title: '',
+        description: '',
+        ingredients: '',
+        instructions: '',
+    });
+
+    useEffect(() => {
+        if (props) {
+            setFormData();
+        }
+    }, [props]);
+
+    return(
+        <div className='row'>
+            <div className='container1-2 background'>
+
+            </div>
+            <div className='container1-2'>
+                
+            </div>
+        </div>
+    )
 }
+export default RecipeForm;
