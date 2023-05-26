b6b7306703dcd8415f6f2ba09fc54f15b1bd37ba
Malachai Rossiter <malachai.rossiter@gmail.com>
2023-05-25 22:41:32 -0400

    worked on search bar

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,18 +1,17 @@
 import NavBar from './NavBar';
+import home from '../styles/home.css';
 
 const Home = (props) => {
 
     const {loggedIn, setLoggedIn} = props;
 
-    const onSubmitHandler => ()
-
     return (
-        <div>
+        <div className='container'>
             <NavBar loggedIn={loggedIn} setLoggedIn={setLoggedIn}/>
             <div className='hero'>
                 <form>
                     <input type='text' className='searchbar'/>
-                    <input type="submit" className='submit-btn'/>
+                    <input type="submit" className='search-btn'/>
                 </form>
             </div>
         </div>
